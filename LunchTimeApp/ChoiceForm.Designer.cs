eda1bf1249046399e0e3288988ad3e525c9fcfcc
YuyaTokeshi <y.tokeshi@dcom-net.jp>
2020-07-21 11:05:09 +0900

    ChoiceFormの追加

---
+++ LunchTimeApp/ChoiceForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace LunchTimeApp
+{
+    partial class ChoiceForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.GenreComboBox = new System.Windows.Forms.ComboBox();
+            this.ResultButton = new System.Windows.Forms.Button();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "今日の気分を選んでください";
+            // 
+            // GenreComboBox
+            // 
+            this.GenreComboBox.FormattingEnabled = true;
+            this.GenreComboBox.Location = new System.Drawing.Point(88, 48);
+            this.GenreComboBox.Name = "GenreComboBox";
+            this.GenreComboBox.Size = new System.Drawing.Size(121, 20);
+            this.GenreComboBox.TabIndex = 1;
+            // 
+            // ResultButton
+            // 
+            this.ResultButton.Location = new System.Drawing.Point(100, 74);
+            this.ResultButton.Name = "ResultButton";
+            this.ResultButton.Size = new System.Drawing.Size(95, 23);
+            this.ResultButton.TabIndex = 2;
+            this.ResultButton.Text = "ランチを決める！";
+            this.ResultButton.UseVisualStyleBackColor = true;
+            // 
+            // BackButton
+            // 
+            this.BackButton.Location = new System.Drawing.Point(219, 108);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(75, 23);
+            this.BackButton.TabIndex = 3;
+            this.BackButton.Text = "戻る";
+            this.BackButton.UseVisualStyleBackColor = true;
+            // 
+            // ChoiceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 141);
+            this.Controls.Add(this.BackButton);
+            this.Controls.Add(this.ResultButton);
+            this.Controls.Add(this.GenreComboBox);
+            this.Controls.Add(this.label1);
+            this.Name = "ChoiceForm";
+            this.Text = "ランチ決めアプリ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox GenreComboBox;
+        private System.Windows.Forms.Button ResultButton;
+        private System.Windows.Forms.Button BackButton;
+    }
+}
